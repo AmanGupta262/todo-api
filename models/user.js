3578c5703df20a1473b49fa5fddd776e7d29f343
Aman Gupta <amang5840@gmail.com>
2021-04-30 20:56:33 +0530

    encrypt user password using bcrypt

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email:{
@@ -18,6 +19,24 @@ const userSchema = new mongoose.Schema({
 },{
     timestamps: true
 });
+
+userSchema.pre(
+    'save',
+    async function(next) {
+        const user = this;
+        const hash = await bcrypt.hash(this.password, 10);
+
+        this.password = hash;
+        next();
+    }
+);
+
+userSchema.methods.isValidPassword = async function(password) {
+    const user = this;
+    const compare = await bcrypt.compare(password, user.password);
+
+    return compare;
+}
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
